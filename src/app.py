8452489e7382cb7f7f4f102dffa29beb5bcddbbd
Ángel Fernández Sobrino <fernandezsobrinoangel@gnail.com>
2024-06-03 16:01:46 +0200

    Major changes
    Add logging system with datetime and level
    Remove exception handling and add retry to task
    Remove online tags discovery and add file import
    Add typehints and improve documentation

--- src/app.py
+++ src/app.py
@@ -1,87 +1,99 @@
-from dotenv import load_dotenv
+# pylint: disable=missing-module-docstring, missing-class-docstring, missing-function-docstring
 
-try: 
-    load_dotenv()
-except Exception as e:
-    print("Could not load .env file", flush=True)
-
+import os
 import json
+import logging
+import sys
+
+import dotenv  # type: ignore
 import paho.mqtt.publish
-import rocketry
+import rocketry  # type: ignore
+import rocketry.conds  # type: ignore
 
 import powerstudio_gateway
 
-import os
+dotenv.load_dotenv()  # type: ignore
 
-powerstudio = os.getenv("POWERSTUDIO")
-powerstudio_port = int(os.getenv("POWERSTUDIO_PORT"))
-broker = os.getenv("MQTT_BROKER")
-port = int(os.getenv("MQTT_PORT"))
-topic = os.getenv("MQTT_TOPIC")
-client_id = os.getenv("MQTT_CLIENT_ID")
-username = os.getenv("MQTT_USERNAME")
-password = os.getenv("MQTT_PASSWORD")
-var_info_retrieval_interval = os.getenv("VAR_INFO_RETRIEVAL_INTERVAL")
-values_retrieval_interval = os.getenv("VALUES_RETRIEVAL_INTERVAL")
 
-print("PowerStudio MQTT Gateway v1.0.0", flush=True)
+LOGGING_LEVEL = os.getenv("LOGGING_LEVEL", "INFO")
+
+POWERSTUDIO_HOST = os.getenv("POWERSTUDIO_HOST", default="http://localhost")
+POWERSTUDIO_PORT = int(os.getenv("POWERSTUDIO_PORT", default="80"))
+
+BROKER = os.getenv("MQTT_BROKER", default="localhost")
+PORT = int(os.getenv("MQTT_PORT", default="1883"))
+TOPIC = os.getenv("MQTT_TOPIC", default="powerstudio")
+CLIENT_ID = os.getenv("MQTT_CLIENT_ID", default="powerstudio-gateway")
+USERNAME = os.getenv("MQTT_USERNAME", default="username")
+PASSWORD = os.getenv("MQTT_PASSWORD", default="password")
+
+VALUES_RETRIEVAL_INTERVAL = os.getenv(
+    "VALUES_RETRIEVAL_INTERVAL", default="5m"
+)
+
+# Configure logging to stdout and stderr
+
+logger = logging.getLogger()
+
+logger.setLevel(LOGGING_LEVEL)
+formatter = logging.Formatter(
+    "%(asctime)s | %(levelname)s | %(message)s", "%m-%d-%Y %H:%M:%S"
+)
+
+stdout_handler = logging.StreamHandler(sys.stdout)
+stdout_handler.setLevel(logging.DEBUG)
+stdout_handler.setFormatter(formatter)
+
+logger.addHandler(stdout_handler)
+
+logger.info("Starting application PowerStudio MQTT Gateway")
 
 # Create gateway object to connect to PowerStudio
-gateway = powerstudio_gateway.PowerStudioGateway(powerstudio, powerstudio_port)
+gateway = powerstudio_gateway.PowerStudioGateway(
+    f"https://{POWERSTUDIO_HOST}", POWERSTUDIO_PORT
+)
 
 # Get all the available tags at application startup
-print('Discovering tags from PowerStudio', flush=True)
-gateway.get_available_tags()
+logger.info("Loading Powerstudio tags file")
+gateway.import_tags("./tags_powerstudio.txt")
+
+if not gateway.tags_available():
+    logger.info("Tags not available, try discovery again")
+    gateway.import_tags("./tags_powerstudio.txt")
+
+if not gateway.tags_available():
+    logger.info("Tags still not available, aborting")
+    sys.exit(1)
 
 # Create Rocketry app instance
 app = rocketry.Rocketry()
 
-# Define tasks
 
-# Discover tags every hour
-@app.task(var_info_retrieval_interval)
-def task_discover_tags():
-    print('Discovering tags from PowerStudio', flush=True)
-    gateway.get_available_tags()
-
-
-# Get data every 5 minutes
-@app.task(values_retrieval_interval)
+# Get data task
+@app.task(VALUES_RETRIEVAL_INTERVAL | rocketry.conds.retry(3))  # type: ignore
 def task_get_data():
-
-    print('Getting data from PowerStudio', flush=True)
-    
-    # Check if tags are available
-    if not gateway.tags_available():
-        print("Tags not available, try discovery again", flush=True)
-        gateway.get_available_tags()
-        if not gateway.tags_available():
-            print("Tags still not available, aborting", flush=True)
-            return
 
     # Get the values from all the variables, in packs
     measurement = gateway.get_tags_values()
     # Print the measurement
     # Convert dictionary to JSON inside a payload object
-    payload_json = json.dumps({'payload': measurement})
+    payload_json = json.dumps({"payload": measurement})
     # Print the JSON
     # Publish to MQTT broker
-    try:
-        paho.mqtt.publish.single(topic,
-                                payload_json,
-                                qos=2,
-                                hostname=broker,
-                                port=port,
-                                client_id=client_id,
-                                auth={
-                                    'username': username,
-                                    'password': password
-                                })
-    except Exception as e:
-        print("Error while publishing to MQTT broker: %s" % e, flush=True)
-    
-    
-    print("Published to MQTT broker %s:%s" % (broker, port), flush=True)
+    logger.info("Retrieved data from Powerstudio, %i tags", len(measurement))
+
+    paho.mqtt.publish.single(
+        TOPIC,
+        payload_json,
+        qos=2,
+        hostname=BROKER,
+        port=PORT,
+        client_id=CLIENT_ID,
+        auth={"username": USERNAME, "password": PASSWORD},
+    )
+
+    logger.info("Published to MQTT broker %s:%i", BROKER, PORT)
+
 
 # Run the app
 app.run()
--- src/powerstudio_gateway.py
+++ src/powerstudio_gateway.py
@@ -1,37 +1,41 @@
+# pylint: disable=missing-class-docstring, missing-function-docstring, line-too-long
+"""powerstudio_gateway module, used to connect to the PowerStudio API and get the data from the devices."""
 
-import json
-import time
+from typing import TypeVar
 import xml.etree
 import xml.etree.ElementTree
 import xml.dom.minidom
 import urllib.request
-from collections import defaultdict
+import urllib.parse
+import concurrent.futures
 
-## PowerStudio API Gateway:
-# This class is used to connect to the PowerStudio API and get the data from the devices.
-# The data is then converted to a dictionary that can be used by the any client for further processing.
-# The class is designed to be used as a singleton, so that the context is only fetched once.
-# The data is fetched in batches to avoid timeouts and performance issues.
-#
-# The gateway has the following methods:
-#
-# -> get_available_tags: Fetches all the available tags from the PowerStudio API
-# ---- Fetch all the devices and their IDs (only once)
-# ---- Fetch all the variables of each device and their IDs (only once)
-#
-# -> get_tags_values: Fetches the values of all the available tags from the PowerStudio API
-# ---- Fetch all the values for all the variables (every time)
-# ---- Convert the data to a dictionary 
-# ---- Return the dictionary
-# 
-# The dictionary is in the following format:
-# {
-#   "variableId": value
-# }
-# 
-# Variable Id is the idEx element of the variable node in the variableInfo XML, which is the id used for fetching the values
 
-class PowerStudioGateway():
+class PowerStudioGateway:
+    """PowerStudio API Gateway:
+    This class is used to connect to the PowerStudio API and get the data from the devices.
+    The data is then converted to a dictionary that can be used by the any client for further processing.
+    The class is designed to be used as a singleton, so that the context is only fetched once.
+    The data is fetched in batches to avoid timeouts and performance issues.
+
+    The gateway has the following methods:
+
+    -> get_available_tags: Fetches all the available tags from the PowerStudio API
+    ---- Fetch all the devices and their IDs (only once)
+    ---- Fetch all the variables of each device and their IDs (only once)
+
+    -> get_tags_values: Fetches the values of all the available tags from the PowerStudio API
+    ---- Fetch all the values for all the variables (every time)
+    ---- Convert the data to a dictionary
+    ---- Return the dictionary
+
+    The dictionary is in the following format:
+    {
+    "variableId": value
+    }
+
+    Variable Id is the idEx element of the variable node in the variableInfo XML, which is the id used for fetching the values
+
+    """
 
     # PowerStudio API paths
     DEVICES_PATH = "/services/user/devices.xml"
@@ -43,121 +47,168 @@ class PowerStudioGateway():
     VARIABLES_PARAM_NAME = "var"
 
     # Batch sizes
-    DISCOVER_VARS_BATCH_SIZE = 24
-    READ_VALUES_BATCH_SIZE = 60
+    DISCOVER_VARS_BATCH_SIZE = 40
 
-    def __init__(self, host, port):
+    """
+    With 180 variables per device it works, and it takes arround 10 seconds.
+    Whatever the number of variables, it will take between 10 and 13 seconds,
+    doesn't seem to be a relation between the number of variables and the time
+    it takes to get all the results.
+    It's also possible that the bottleneck is the python code itself, and not
+    the PowerStudio server, probably because of the XML parsing. A way to
+    improve this would be to use a more "low level" approach, like using the
+    file descriptor directly to read the XML, as we already know the structure
+    of the XML.
+    """
+    READ_VALUES_BATCH_SIZE = 40
+
+    def __init__(self, host: str, port: int):
         self.host = host
         self.port = port
-        self.variable_ids = None
+        self.variable_ids: list[str] = []
 
     def tags_available(self):
-        return self.variable_ids != None
+        return len(self.variable_ids) > 0
+
+    def import_tags(self, tags_file_path: str) -> None:
+
+        with open(tags_file_path, "r", encoding="UTF-8") as tags_file:
+
+            line = tags_file.readline()
+
+            while line:
+
+                if line[0] != "#" and line[0] != "\n" and len(line) > 2:
+                    self.variable_ids.append(line.strip())
+
+                line = tags_file.readline()
 
     def get_available_tags(self):
         # Get all the devices XML from PowerStudio
-        try:
-            response = urllib.request.urlopen(f'{self.host}:{self.port}{PowerStudioGateway.DEVICES_PATH}').read()
-        except Exception as e:
-            print("Could not connect to PowerStudio", flush=True)
-            print("Error: ", e, flush=True)
-            return
-        
-        # Parse the XML
-        devices = xml.etree.ElementTree.fromstring(response.decode('utf-8'))
+        response = urllib.request.urlopen(
+            f"{self.host}:{self.port}{PowerStudioGateway.DEVICES_PATH}"
+        ).read()
+
+        devices = xml.etree.ElementTree.fromstring(response.decode("utf-8"))
 
         # Get the device IDs
-        device_ids = []
+        device_ids: list[str] = []
         for device in devices:
-            device_ids.append(device.text)
-        
-        print("Found %i devices" % len(device_ids), flush=True)
-       
-        # Get the variables from all the devices, in packs, to avoid timeouts
-        packs = chop(device_ids, PowerStudioGateway.DISCOVER_VARS_BATCH_SIZE)
-        variable_ids = []
-        for pack in packs:
+            if device.text is not None:
+                device_ids.append(device.text)
 
+        # Get the variables from all the devices, in packs, to avoid timeouts
+        packs: list[list[str]] = make_batch_consecutive_sized(
+            device_ids, PowerStudioGateway.DISCOVER_VARS_BATCH_SIZE
+        )
+        variable_ids: list[str] = []
+
+        def get_variables(pack: list[str]) -> list[str]:
             # Build the URI
-            variables_uri = f'{self.host}:{self.port}{self.VARIABLES_PATH}?'
-            for varId in pack:
+            variables_uri = f"{self.host}:{self.port}{self.VARIABLES_PATH}?"
+            for device_id in pack:
                 variables_uri += urllib.parse.quote(
-                    f'{self.DEVICES_PARAM_NAME}={varId}&', encoding='utf-8')
+                    f"{self.DEVICES_PARAM_NAME}={device_id}&", encoding="utf-8"
+                )
 
             # Get the variables info XML from PowerStudio
-            try:
-                response = urllib.request.urlopen(variables_uri).read()
-            except Exception as e:
-                print("Could not connect to PowerStudio", flush=True)
-                print("Error: ", e, flush=True)
-                return
+            response = urllib.request.urlopen(variables_uri).read()
 
             # Parse the XML
             variables = xml.etree.ElementTree.fromstring(
-                response.decode('utf-8'))
-            
-            # Get the variable IDs
-            variable_ids = []
+                response.decode("utf-8")
+            )
+            result: list[str] = []
             for variable in variables:
-                variable_ids.append(variable.find('idEx').text)
+                id_ex_element = variable.find("idEx")
+                if (
+                    id_ex_element is not None
+                    and id_ex_element.text is not None
+                ):
+                    result.append(id_ex_element.text)
+
+            return result
+
+        with concurrent.futures.ThreadPoolExecutor(
+            max_workers=100
+        ) as executor:
+            # Start the load operations and mark each future with its URL
+            futures = {executor.submit(get_variables, pack) for pack in packs}
+            for future in concurrent.futures.as_completed(futures):
+
+                result = future.result()
+                variable_ids += result
 
         # Store the variable IDs in the instance for later use
         self.variable_ids = variable_ids
 
-    def get_tags_values(self):
-        measurements = defaultdict(float)
+    def get_tags_values(self) -> dict[str, float]:
+        measurements: dict[str, float] = {}
 
-        if self.variable_ids is None:
-            print("No variables found", flush=True)
+        if not self.tags_available():
+            raise ValueError("No tags available")
 
         # Get the values from all the variables, in packs, to avoid timeouts
-        packs = chop(self.variable_ids, PowerStudioGateway.READ_VALUES_BATCH_SIZE)
-        for pack in packs:
+        batches = make_batch_consecutive_sized(
+            self.variable_ids, PowerStudioGateway.READ_VALUES_BATCH_SIZE
+        )
 
+        def get_values(batch: list[str]) -> dict[str, float]:
+            measurements: dict[str, float] = {}
             # Build the URI
-            variables_uri = f'{self.host}:{self.port}{self.VALUES_PATH}?'
-            for varId in pack:
-                variables_uri += f'{self.VARIABLES_PARAM_NAME}={varId}&'
+            values_uri = f"{self.host}:{self.port}{self.VALUES_PATH}?"
+            for variable_id in batch:
+                values_uri += f"{self.VARIABLES_PARAM_NAME}={variable_id}&"
+
+            values_uri = values_uri.replace(" ", "%20")
 
             # Get the variables values XML from PowerStudio
-            try:
-                response = urllib.request.urlopen(variables_uri.replace(
-                " ", "%20")).read()
-            except Exception as e:
-                print("Could not connect to PowerStudio", flush=True)
-                print("Error: ", e, flush=True)
-                return []
-            
+            response = urllib.request.urlopen(values_uri).read()
+
             # Parse the XML
-            values = xml.etree.ElementTree.fromstring(response.decode('utf-8'))
+            values = xml.etree.ElementTree.fromstring(response.decode("utf-8"))
 
             # Get the variable IDs and values and store them in the measurements dict
             for value in values:
-                if value is not None:
-                    id = value.find('id')
-                    value = value.find('value')
-                    if value is not None and id is not None:
-                        measurements[id.text] = value.text
+                variable_id = value.find("id")
+                value = value.find("value")
+                if (
+                    value is not None
+                    and variable_id is not None
+                    and variable_id.text is not None
+                    and value.text is not None
+                ):
+                    measurements[variable_id.text] = (
+                        float(value.text) if value.text != "-" else 0.0
+                    )
+
+            return measurements
+
+        with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+            # Start the load operations and mark each future with its URL
+            futures = {executor.submit(get_values, pack) for pack in batches}
+            for future in concurrent.futures.as_completed(futures):
+
+                result = future.result()
+                measurements.update(result)
 
         # Return the measurements dict
         return measurements
 
 
+T = TypeVar("T")
+
+
 # Split a list into chunks of a given size
-def chop(list, size):
+def make_batch_consecutive_sized(
+    elements_list: list[T], size: int
+) -> list[list[T]]:
 
     # Check if the size is valid
     if size < 2:
         raise ValueError("Chop size cannot be lower than 2")
 
-    ## Split the list into chunks
-    # Create an empty list to store the chunks
-    parts = []
-    # Get the length of the list
-    N = len(list)
-    # Loop through the list for each chunk
-    for i in range(0, N, size):
-        # Slice the list and append it to the parts list
-        # If the chunk is the last one, slice the list until the end using the min function to avoid out of range errors
-        parts.append(list[i:min(N, i + size)])
-    return parts
+    # Split the list into chunks
+    return [
+        elements_list[i : i + size] for i in range(0, len(elements_list), size)
+    ]
